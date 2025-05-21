b30f4a6a2c6fab70342607a613942914c0eb0831
Sebapu <sebas.puppo@hotmail.com>
2025-05-21 11:56:01 -0300

    correccion prop boton

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,41 +1,33 @@
 'use client';
 
+import Link from 'next/link';
 import React from 'react';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps {
+  href: string;
+  children: React.ReactNode;
   variant?: 'primary' | 'secondary';
-  href?: string;
+  className?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
+  href,
+  children,
   variant = 'primary',
   className = '',
-  children,
-  href,
-  ...rest
 }) => {
   const baseStyle =
-    'px-4 py-2 rounded-lg font-semibold border border-[1px] transition-colors duration-200';
+    'inline-block px-4 py-2 rounded-lg font-semibold border border-[1px] transition-colors duration-200';
 
   const variantStyles: { [key in NonNullable<ButtonProps['variant']>]: string } = {
     primary: 'bg-transparent text-black border-gray-400 hover:bg-gray-100',
     secondary: 'bg-transparent text-[#800020] border-[#800020] hover:bg-[#f3d6da]',
   };
 
-  const combinedClassName = `${baseStyle} ${variantStyles[variant]} ${className}`;
-
-  if (href) {
-    return (
-      <a href={href} className={combinedClassName}>
-        {children}
-      </a>
-    );
-  }
-
   return (
-    <button className={combinedClassName} {...rest}>
+    <Link href={href} className={`${baseStyle} ${variantStyles[variant]} ${className}`}>
       {children}
-    </button>
+    </Link>
   );
 };
 
